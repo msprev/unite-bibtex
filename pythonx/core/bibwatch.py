a208195a8846394168a28aff94c9910e604a8496
Mark Sprevak <sprevak.home@gmail.com>
2015-09-25 17:15:11 +0100

    add more warnings to bibwatch

--- pythonx/core/bibwatch.py
+++ pythonx/core/bibwatch.py
@@ -34,37 +34,37 @@ def main():
     mtime_cur = dict()
     # read command line arguments
     cli_args = parse_cli()
+    if not cli_args:
+        exit(1)
     bib_files = cli_args['input']
     cache_dir = cli_args['cache']
-    if not bib_files:
-        print('No BibTeX files listed on command line to monitor')
-        exit(1)
-    if not cache_dir:
-        print('No cache directory specified on command line')
-        exit(2)
-    # check if cache is up to date
-    for b in bib_files:
-        print('monitoring: "%s"' % b)
-        c[b] = cache.Cache(b, cache_dir)
-        mtime_last[b] = 0
-        try:
-            c[b].read()
-            mtime_last[b] = c[b].timestamp
-        except (cache.NoCache, cache.OutdatedCache):
-            mtime_last[b] = 0
-    print('cache directory: "%s"' % cache_dir)
-    print('(press Control+C to stop monitoring)')
-    # monitor for changes
     try:
+        # check if cache is up to date
+        for b in bib_files:
+            print('monitoring: "%s"' % b)
+            c[b] = cache.Cache(b, cache_dir)
+            mtime_last[b] = 0
+            try:
+                c[b].read()
+                mtime_last[b] = c[b].timestamp
+            except (cache.NoCache, cache.OutdatedCache):
+                mtime_last[b] = 0
+        print('cache directory: "%s"' % cache_dir)
+        print('(press Control+C to stop monitoring)')
+        # monitor for changes
         while True:
             for b in bib_files:
                 mtime_cur[b] = os.path.getmtime(b)
                 if mtime_cur[b] != mtime_last[b]:
                     update(c[b])
-                mtime_last[b] = mtime_cur[b]
-                time.sleep(SLEEP_TIME)
+                    mtime_last[b] = mtime_cur[b]
+                    time.sleep(SLEEP_TIME)
     except KeyboardInterrupt:
         print('\nStopped')
+    except cache.MissingSource:
+        print('ERROR: file not found')
+    except cache.MissingCacheDir:
+        print('ERROR: cache directory not found')
 
 def update(c):
     today = datetime.date.today().isoformat()
@@ -85,19 +85,23 @@ def parse_cli():
         epilog=EPILOG,
         formatter_class=argparse.RawTextHelpFormatter,
         add_help=False)
-    p.add_argument('input', nargs='*',
-                   help="BibTeX files to monitor for changes")
-    p.add_argument("-h", "--help", '---help', '---h',
-                   action="help",
+    p.add_argument('input', nargs='*', help="BibTeX files to monitor for changes")
+    p.add_argument("-h", "--help", '---help', '---h', action="help",
                    help="show this help message and exit")
-    p.add_argument('-v', '--version', '---version', '---v',
-                   action='version',
+    p.add_argument('-v', '--version', '---version', '---v', action='version',
                    version=('%(prog)s ' + VERSION))
-    p.add_argument("--cache", '-c',
-                    help='cache directory to write cached files')
+    p.add_argument("--cache", '-c', help='cache directory to write cached files')
     known_raw, unknown = p.parse_known_args()
-    known = vars(known_raw)
-    return known
+    args = vars(known_raw)
+    if not args['input']:
+        print('ERROR: No BibTeX files listed on command line to monitor\n')
+        p.print_help()
+        return None
+    if not args['cache']:
+        print('ERROR: No cache directory specified on command line\n')
+        p.print_help()
+        return None
+    return args
 
 if __name__ == '__main__':
     main()
